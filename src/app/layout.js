a594b4add1646be56f2ec7d7f2bb0a27d174f112
Terry Kwok <terrykwok147@gmail.com>
2025-02-05 20:00:13 -0800

    updating scale

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} scale-88`}>
+      <body className={`${inter.className} scale-90`}>
         <Suspense fallback={<Loading />}>{children}</Suspense>
       </body>
     </html>
